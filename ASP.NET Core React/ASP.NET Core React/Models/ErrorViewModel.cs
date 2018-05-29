ad7737eec4fdc7f7d19d330ee046d58be9c2aa1d
raconteur <sewa080@gmail.com>
2018-05-29 10:57:45 +0300

    Initial

---
+++ ASP.NET Core React/ASP.NET Core React/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Core_React.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
